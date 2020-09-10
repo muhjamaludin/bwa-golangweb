d7b58efce819f7fe5d61a2551ac1a79b23cfd502
74maludin <74maludin@gmail.com>
2020-09-10 21:29:06 +0700

    mengambil query string

--- main.go
+++ main.go
@@ -1,24 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
-	aboutHandler := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("About Page"))
-	}
-
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
 	mux.HandleFunc("/mario", marioHandler)
-	mux.HandleFunc("/about", aboutHandler)
-	mux.HandleFunc("/profile", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Profile Page"))
-	})
+	mux.HandleFunc("/product", productHandler)
 
 	log.Println("starting web on port 8080")
 	err := http.ListenAndServe(":8080", mux)
@@ -40,4 +35,16 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func marioHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world, saya mario dari nintendo"))
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	idNumb, err := strconv.Atoi(id)
+	if err != nil && idNumb < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Product page: %d", idNumb)
 }
