055030732c7b5d432665fb01bf97302fe4c0bcaf
74maludin <74maludin@gmail.com>
2020-09-10 21:49:13 +0700

    menampilkan halaman html

--- handler/handler.go
+++ handler/handler.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path"
 	"strconv"
 )
 
@@ -12,7 +15,18 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("'Welcome to Home'"))
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"))
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening, keep calm", http.StatusInternalServerError)
+		return
+	}
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
