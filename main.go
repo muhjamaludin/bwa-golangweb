7ad31b099c443a1c9ebaf521addb2015b0d48ccc
74maludin <74maludin@gmail.com>
2020-09-10 21:11:23 +0700

    request routing

--- main.go
+++ main.go
@@ -8,13 +8,28 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/hello", helloHandler)
+	mux.HandleFunc("/mario", marioHandler)
 
 	log.Println("starting web on port 8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("'Welcome to Home'"))
+}
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world, saya sedang belajar golang cuy"))
+}
+
+func marioHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world, saya mario dari nintendo"))
 }
