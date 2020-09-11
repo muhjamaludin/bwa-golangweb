bc32191ce6bf9193a3c88fbcc642065042868dff
74maludin <74maludin@gmail.com>
2020-09-11 07:38:41 +0700

    menggunakan static file

--- main.go
+++ main.go
@@ -14,6 +14,9 @@ func main() {
 	mux.HandleFunc("/mario", handler.MarioHandler)
 	mux.HandleFunc("/product", handler.ProductHandler)
 
+	fileServer := http.FileServer(http.Dir("assets"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("starting web on port 8080")
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
